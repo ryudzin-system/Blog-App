afe2acda66d2acfb825efe369687714f09fed340
Akhror Tursunov <akhror.anvarovich@gmail.com>
2023-03-02 11:43:53 -0500

    second commit about Forms

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
+
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
@@ -9,4 +12,18 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-    
+class BlogCreateNew(CreateView):
+    model = Post
+    template_name = 'post_new.html' 
+    fields = [ 'title', 'author', 'body']  
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
